93a052617ff6fe0306536b0c26ece2e1acde42f9
rohandagale <workspacex28@gmail.com>
2024-10-07 15:32:46 +0530

    deployment url

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ const { verifyToken } = require("./middleware/VerifyToken");
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173", // The frontend domain
+    origin: "https://notify-chat-application-server.vercel.app", // The frontend domain
     credentials: true, // Allow credentials (cookies)
   })
 );
