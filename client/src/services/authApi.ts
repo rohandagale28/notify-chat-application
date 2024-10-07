562fbcd3a75bb867156d9839ff23ef7ac3ba21fc
rohandagale <workspacex28@gmail.com>
2024-10-07 18:07:06 +0530

    deployment url

--- client/src/services/authApi.ts
+++ client/src/services/authApi.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = "https://notify-chat-application-server.vercel.app" //base url  ----> IMP
+const url = "https://notify-chat-application-serv-git-6dffe8-rohandagale28s-projects.vercel.app" //base url  ----> IMP
 
 
 //==========|| login function ||==========//
