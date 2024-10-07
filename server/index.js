cd0bb3c64672b02a49a2a5f0c94cb72ea150d711
rohandagale <workspacex28@gmail.com>
2024-10-07 19:43:29 +0530

    testing deployments

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ const corsOptions = {
 };
 
 function setCorsHeaders(req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', 'https://notify-chat-application-clie-git-489a45-rohandagale28s-projects.vercel.app');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
